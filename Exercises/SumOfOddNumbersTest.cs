17c86457fd4f55950d2ac7c8d84b4a64353e6a0e
Luis Manuel González <luismanuelglz.dev@gmail.com>
2022-08-03 00:10:24 -0700

    Add project files.

---
+++ Exercises/SumOfOddNumbersTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Exercises
+{
+    [TestFixture]
+    public class SumOfOddNumbersTest
+    {
+        [Test]
+        [TestCase(1, 1)]
+        [TestCase(74088, 42)]
+        [TestCase(148877, 53)]
+        [TestCase(1000000, 100)]
+        public void Test(long expected, long n)
+        {
+            Assert.AreEqual(expected, SumOfOddNumbers.RowSumOddNumbers(n));
+        }
+    }
+}
